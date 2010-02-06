ae3696739612e0b0f1080b9c51c253f7be0f99a7
Tom Wilson <tom@jackhq.com>
2010-02-06 09:06:51 -0500

    forgot activeresource-ext file

---
+++ lib/activeresource-ext.rb
@@ -0,0 +1,42 @@
+require 'activeresource'
+
+module ActiveResource
+  module CaptureHeaders
+    module InstanceMethods
+      private
+      def request_with_headers(method, path, *arguments)
+        request_without_headers(method, path, *arguments).tap do |resp|
+          @headers = resp.to_hash
+        end
+      end
+
+    end
+
+    def self.included(receiver)
+      receiver.class_eval do
+        include InstanceMethods
+        alias_method_chain :request, :headers
+        attr :headers
+      end
+    end
+  end
+  
+  module RetrieveHeaders
+    module InstanceMethods
+      def last_count
+        @last_count ||= (c = connection.headers['x-query-count']) ? c[0].to_i : nil
+      end
+      
+    end
+    
+    def self.included(receiver)
+      receiver.class_eval do
+        include InstanceMethods
+      end
+    end
+  end
+  
+end
+
+ActiveResource::Connection.send :include, ActiveResource::CaptureHeaders
+ActiveResource::Base.send :include, ActiveResource::RetrieveHeaders
--- zerigo_dns.gemspec
+++ zerigo_dns.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{zerigo_dns}
-  s.version = "1.0.4"
+  s.version = "1.0.5"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Tom Wilson"]
@@ -21,6 +21,7 @@ Gem::Specification.new do |s|
      "README.rdoc",
      "Rakefile",
      "example/sample.rb",
+     "lib/activeresource-ext.rb",
      "lib/zerigo_dns.rb"
   ]
   s.homepage = %q{http://github.com/twilson63/zerigo_dns}
